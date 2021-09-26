53f330ee520121abc3a183bfc378d2b6c9ecfd31
Peter Koenders <pkoenders@gmail.com>
2021-09-26 17:20:08 +1300

    aa

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -23,7 +23,7 @@ export const wrapRootElement = ({ element }) => (
 // Provide an update message
 export const onServiceWorkerUpdateReady = () => {
   const answer = window.confirm(
-    'This application has been updated. Reload to display the latest version?'
+    'This application has been updated. Reload to display the latest version'
   )
   if (answer === true) {
     window.location.reload()
