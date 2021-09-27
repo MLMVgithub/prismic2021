b43616107dbc6e40f632e5b691972f543f6a46c1
Peter Koenders <pkoenders@gmail.com>
2021-09-27 21:00:27 +1300

    aa

--- src/components/slices/seo/index.js
+++ src/components/slices/seo/index.js
@@ -8,14 +8,6 @@ const SEOIndex = ({ slice }) => {
   return (
     <Helmet>
       <html lang="en-nz" />
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-S55JE9KKJR"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-S55JE9KKJR');
-</script>
       <General />
       <OpenGraph />
       <Twitter />
