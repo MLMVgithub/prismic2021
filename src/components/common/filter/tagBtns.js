a47ac38e35e8128e1733c4dc0a4df398ffb4737c
Peter Koenders <pkoenders@gmail.com>
2021-09-30 21:53:43 +1300

    aa

--- src/components/common/filter/tagBtns.js
+++ src/components/common/filter/tagBtns.js
@@ -267,6 +267,7 @@ const ListTagBtns = ({ resetFilterBtns, tagList, resetCards, resetSearchQuery })
           {tagList.map((node, index) => (
             <button
               className="tagButton"
+              type="button"
               id={_.camelCase(node)}
               key={`tagButton-` + index}
               onMouseDown={resetCards}
