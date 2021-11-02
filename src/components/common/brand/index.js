c06517e3371526f520ff7b91a74129957e3f7fc8
Peter Koenders <pkoenders@gmail.com>
2021-11-03 00:58:30 +1300

    aa

--- src/components/common/brand/index.js
+++ src/components/common/brand/index.js
@@ -4,6 +4,6 @@ import i18n from '/config/i18n'
 import Img from '/src/images/svg/brand-mlmv.inline.svg'
 
 const Brand = ({ currentLang }) => {
-  return <Img alt={`${i18n[currentLang].logo} - ${i18n[currentLang].siteTitle}`} />
+  return <Img alt={`${i18n[currentLang].logo} - ${i18n[currentLang].siteTitle}`} role="img" />
 }
 export default Brand
