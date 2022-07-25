bd8d2abccc346591f6d07bdf1f8f289c65513785
Peter Koenders <pkoenders@gmail.com>
2022-07-25 22:27:55 +1200

    update banner component

--- src/components/slices/heroImage/index.js
+++ src/components/slices/heroImage/index.js
@@ -33,7 +33,6 @@ const HeroImage = styled.section.attrs({
   padding-bottom: 0px;
   z-index: 100;
   height: min-content;
-  
 
   > div {
     max-width: ${({ theme }) => theme.screens.md};
@@ -78,7 +77,6 @@ const HeroImage = styled.section.attrs({
       align-items: center;
       margin: 0 auto;
       padding: ${({ theme }) => theme.padding.default} ${({ theme }) => theme.padding['1/2']};
-      
     }
     .contentWrapper.centre {
       align-items: center;
@@ -95,7 +93,6 @@ const HeroImage = styled.section.attrs({
     }
 
     .contentWrapper {
-      
       .content,
       .content.centre {
         margin: 0 auto;
@@ -142,67 +139,60 @@ const HeroImage = styled.section.attrs({
         hyphens: none;
 
         @media (max-width: ${({ theme }) => theme.screens.sm}) {
-          padding: ${({ theme }) => theme.padding['1/2']}};
+          padding: ${({ theme }) => theme.padding['1/2']};
         }
-        
-       
+      }
 
-        span * {
-          margin: 0;
-        }
+      span * {
+        margin: 0;
+      }
 
-        span.hide {
-          display:none;
-        }
-        span {
-          display: grid;
-          grid-gap: ${({ theme }) => theme.padding['1/2']};
-           
-          .leadImage {
-            display: flex;
-            max-width: fit-content;
-           /* min-width:auto !important; */
-           object-fit: contain !important;
-            margin: ${({ theme }) => theme.padding['1/2']} auto 0 auto;
-            > div {
-               max-width: 100% !important;
-               /* max-width:auto !important */
-            }
-            img {
-               object-fit: contain !important;
-            }
-          }
-          
+      span {
+        display: grid;
+        grid-gap: ${({ theme }) => theme.padding['1/2']};
 
-
-          h1 {
-            overflow-wrap: break-word;
-            word-wrap: break-word;
-            hyphens: auto;
-          }
-
-          p {
-            color: inherit;
-            width: 100%;
-            margin-bottom: 0px;
-            font-size: 115%;
-
-            a,
-            a:hover {
-              color: #ffffff;
-              text-decoration: underline;
-            }
-          }
-        }
-
-        span.cta {
+        .leadImage {
           display: flex;
-          grid-gap: ${({ theme }) => theme.padding['1/2']};
-          width: fit-content;
-
-          @media (max-width: ${({ theme }) => theme.screens.sm}) {
-            flex-direction: column;
+          max-width: fit-content;
+          /* min-width:auto !important; */
+          object-fit: contain !important;
+          margin: ${({ theme }) => theme.padding['1/2']} auto 0 auto;
+          > div {
+            max-width: 100% !important;
+            /* max-width:auto !important */
           }
+          img {
+            object-fit: contain !important;
+          }
+        }
+
+        h1 {
+          overflow-wrap: break-word;
+          word-wrap: break-word;
+          hyphens: auto;
+        }
+
+        p {
+          color: inherit;
+          width: 100%;
+          margin-bottom: 0px;
+          font-size: 115%;
+
+          a,
+          a:hover {
+            color: #ffffff;
+            text-decoration: underline;
+          }
+        }
+      }
+
+      span.cta {
+        display: flex;
+        grid-gap: ${({ theme }) => theme.padding['1/2']};
+        width: fit-content;
+
+        @media (max-width: ${({ theme }) => theme.screens.sm}) {
+          flex-direction: column;
         }
       }
     }
@@ -406,7 +396,7 @@ const HeroImg = ({ slice }) => {
                 )}
 
                 {title && title[0].text.length > 0 && (
-                  <span className={slice.primary.display_title === false && 'hide'}>
+                  <span className={slice.primary.display_title === true && 'sr-only'}>
                     <RichText render={title} />
                   </span>
                 )}
