baa3c8f67da7c2889a82d82a6d6fc62e06872925
Peter Koenders <pkoenders@gmail.com>
2022-07-31 01:26:56 +1200

    aa

--- src/components/common/filter/sortList.js
+++ src/components/common/filter/sortList.js
@@ -914,7 +914,7 @@ const Sort = ({
           <IconMaterial icon={'expand_more'} />
         </button>
 
-        <ul
+        <ul // eslint-disable-line jsx-a11y/no-static-element-interactions
           id="list_box"
           tabIndex="-1"
           role="listbox"
@@ -922,7 +922,7 @@ const Sort = ({
           className="sortBox hidden"
         >
           {items.map((node, i) => (
-            <li
+            <li // eslint-disable-line jsx-a11y/no-static-element-interactions
               id={'list-item'[i]}
               role="option"
               aria-selected="false"
